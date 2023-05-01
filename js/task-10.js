ab4a0cd358e372519d3dfe278598d910cbfcacd2
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-05-02 00:48:42 +0300

    ok

--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,15 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+const contr = document.body
+console.log(`contr`)
+
+// function createBoxes(amount){
+
+// }
+
+
+
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215)
+//     .toString(16)
+//     .padStart(6, 0)}`;
+// }
