6e7f121eac2cdd28cacbc3afe28e13083cb32f34
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-05-05 02:22:43 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -1,15 +1,58 @@
-const contr = document.body
-console.log(`contr`)
+const input = document.querySelector('[type="number"]')
+console.log(input)
 
-// function createBoxes(amount){
+const btnCreate = document.querySelector('[data-create]')
+console.log(btnCreate)
 
-// }
+const btnDestroy = document.querySelector('[data-destroy]')
+console.log(btnDestroy)
+
+const boxes = document.querySelector('#boxes')
+console.log(boxes)
 
 
 
+btnCreate.addEventListener('click', onClickCreateBtn => {
+let amount = Number(input.value)
+createBoxes(amount)
+})
 
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
+btnDestroy.addEventListener('click', onClickDectroyBtn => destroyBoxes())
+
+
+
+function createBoxes(amount) {
+    console.log(amount)
+    boxes.innerHTML = ''
+   let arrDiv = [];
+
+    for(let i=0; i < amount; i+=1){
+        const box = document.createElement('div')
+        const size = 30 + 10 * i 
+
+        box.style.width = size + 'px'
+        box.style.height = size + 'px'
+        box.style.backgroundColor = getRandomHexColor()
+
+        arrDiv.push(box)
+        console.log(i, arrDiv)
+    }
+
+    boxes.append(...arrDiv)
+    console.log(boxes)
+
+}
+ 
+
+function destroyBoxes() {
+    boxes.innerHTML = ''
+    input.value = ''
+    console.log(boxes)
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
